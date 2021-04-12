7c45862ca9fac350e9f5e7dbc29bf1ce652135c4
hjwforever <hjwbelieve@foxmail.com>
2021-04-12 21:55:44 +0800

    init project

---
+++ src/main/java/com/aruoxi/ebookshop/EbookshopApplication.java
@@ -0,0 +1,13 @@
+package com.aruoxi.ebookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aruoxi/ebookshop/EbookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aruoxi.ebookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
