3cb7b60947d95e304c127e2382f32161b52abc7f
hjwforever <hjwbelieve@foxmail.com>
2021-04-14 13:05:25 +0800

    finish APIs for Book

--- src/test/java/com/aruoxi/ebookshop/EbookshopApplicationTests.java
+++ src/test/java/com/aruoxi/ebookshop/EbookshopApplicationTests.java
@@ -1,13 +1,28 @@
 package com.aruoxi.ebookshop;
 
+import com.aruoxi.ebookshop.domain.Book;
+import com.aruoxi.ebookshop.repository.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class EbookshopApplicationTests {
+	private static final Logger LOG = LoggerFactory.getLogger(EbookshopApplicationTests.class);
+
+	@Resource
+	private BookRepository bookRepository;
 
 	@Test
 	void contextLoads() {
+		Sort sort = Sort.by(Sort.Direction.DESC, "create_time");
+		Page<Book> books = bookRepository.findByNameLike("点", PageRequest.of(0, 2, sort));
+		LOG.info("books = " + books);
 	}
-
 }
